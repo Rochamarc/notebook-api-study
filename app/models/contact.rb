a730fb243298bc605edebfe7016c66231f28c49a
nuke <rochadcmarcos@gmail.com>
2020-08-11 09:59:02 -0300

    new scaffold and references, changes on model contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ApplicationRecord
+  belongs_to :kind
+
+  def author
+    "Marcos Rocha"
+  end
+
+  def kind_description
+    self.kind.description
+  end
+  def as_json(options={})
+    # Invoca o metodo mae, e passa somente os metodos que voce deseja
+    super(
+      root: true,
+      methods: [:author, :kind_description]
+      )
+  end
 end
