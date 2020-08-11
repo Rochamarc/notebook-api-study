e36dad3cd0247e2ee0d85095b6c6477044c36cc3
nuke <rochadcmarcos@gmail.com>
2020-08-11 10:39:49 -0300

    optional and unpermited parameters

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,19 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind #, optional: true
 
-  def author
-    "Marcos Rocha"
-  end
+  #def author
+  #  "Marcos Rocha"
+  #end
+  #
+  #def kind_description
+  #  self.kind.description
+  #end
+  #def as_json(options={})
+  #  # Invoca o metodo mae, e passa somente os metodos que voce deseja
+  #  super(
+  #    root: true,
+  #    methods: [:author, :kind_description]
+  #    )
+  #end
 
-  def kind_description
-    self.kind.description
-  end
-  def as_json(options={})
-    # Invoca o metodo mae, e passa somente os metodos que voce deseja
-    super(
-      root: true,
-      methods: [:author, :kind_description]
-      )
-  end
 end
