3f3501c377da4d6f6e8e40fd377d366973116dc8
nuke <rochadcmarcos@gmail.com>
2020-08-12 13:43:28 -0300

    permitted unpermited parameters, allow destroying phones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,8 @@
 class Contact < ApplicationRecord
-  # Associations
   belongs_to :kind #, optional: true
   has_many :phones
+  # Aceita atributos dos telefones
+  accepts_nested_attributes_for :phones, allow_destroy: true 
 
   #Sobrescreve a funcao as_json
   def as_json(options={})
@@ -10,28 +11,28 @@ class Contact < ApplicationRecord
     h
   end
 
-  #def to_br
-  #  {
-  #    name: self.name,
-  #    email: self.email,
-  #    birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-  #  }
-  #end
-#
-  #def author
-  #  "Marcos Rocha"
-  #end
-  #
-  #def kind_description
-  #  self.kind.description
-  #end
-  #def as_json(options={})
-  #  # Invoca o metodo mae, e passa somente os metodos que voce deseja
-  #  super(
-  #    root: true,
-  #    methods: [:author, :kind_description]
-  #    )
-  #end
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }
+  # end
+# 
+  # def author
+  #   "Marcos Rocha"
+  # end
+  # 
+  # def kind_description
+  #   self.kind.description
+  # end
+  # def as_json(options={})
+  #   # Invoca o metodo mae, e passa somente os metodos que voce deseja
+  #   super(
+  #     root: true,
+  #     methods: [:author, :kind_description]
+  #     )
+  # end
 
   def hello
     I18n.t('hello')
