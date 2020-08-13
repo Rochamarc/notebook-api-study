b14ffef0bd1100696b097c425926b6e9444b2e1a
nuke <rochadcmarcos@gmail.com>
2020-08-13 10:43:56 -0300

    serializer implemented

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,12 +6,12 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true 
   accepts_nested_attributes_for :address, update_only: true  # Singular pois cada contato possui somente um endereço, so permite atualizacao de endereço unico
 
-  #Sobrescreve a funcao as_json
-  def as_json(options={})
-    h = super(options)
-    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    h
-  end
+  # Sobrescreve a funcao as_json
+  # def as_json(options={})
+  #   h = super(options)
+  #   h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   h
+  # end
 
   # def to_br
   #   {
