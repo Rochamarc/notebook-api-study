613c11b8eccdce930c05e9122e2dc0b51aa9ef87
nuke <rochadcmarcos@gmail.com>
2020-08-14 10:50:08 -0300

    register the vnd.api+json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
     before_filter :ensure_json_request
 
     def ensure_json_request
-        return if request.headers['Accept'] =~ /json/
+        return if request.headers['Accept'] =~ /vnd\.api\+json/
         render :nothing => true, :status => 406
     end
 end
