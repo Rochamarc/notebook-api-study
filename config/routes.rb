d2e5fe521a6ce8f57f8f38650b421aa5d7027c42
nuke <rochadcmarcos@gmail.com>
2020-08-15 14:04:52 -0300

    add link relationships (has_one)  and routes to address

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  # get 'addresses/show'
+
   resources :kinds
   
   resources :contacts do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
+    
+    resource :address, only: [:show]
+    resource :address, only: [:show], path: 'relationships/address' 
+    
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/phones'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
