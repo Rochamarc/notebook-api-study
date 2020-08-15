2bfd6bbd371a26ac1dd5ab0a87ad0b5c756136ce
nuke <rochadcmarcos@gmail.com>
2020-08-15 15:54:00 -0300

    deprecation warning ajusted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     # Garante que as requisições sejam do tipo json
-    before_filter :ensure_json_request
+    before_ action:ensure_json_request
 
     def ensure_json_request
         return if request.headers['Accept'] =~ /vnd\.api\+json/
