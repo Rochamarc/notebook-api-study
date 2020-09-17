40e5b438fe24aed290f33a36dd05c1d3eb0e083c
nuke <rochadcmarcos@gmail.com>
2020-09-17 11:16:54 -0300

    refactor: use X-Version on the header

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :kinds
   
   # Pega a versao da mesma maneira que query parameter, porem atraves da lib versionist
-  api_version(:module => "V1", :parameter => {:name => "version", :value => "1"}) do 
+  api_version(:module => "V1", :header => {:name => "X-Version", :value => "1.0"}) do 
     resources :contacts do 
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
     end
   end
 
-  api_version(:module => "V2", :parameter => {:name => "version", :value => "2"}) do 
+  api_version(:module => "V2", :header => {:name => "X-Version", :value => "2.0"}) do 
     resources :contacts do  
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
