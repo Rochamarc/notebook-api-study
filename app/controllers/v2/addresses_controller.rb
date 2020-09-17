67b1f0f5b958aad93185ee5fc6cac9d4d5da93dd
nuke <rochadcmarcos@gmail.com>
2020-09-17 09:48:48 -0300

    feat: add module V2 to controllers

--- app/controllers/v2/addresses_controller.rb
+++ app/controllers/v2/addresses_controller.rb
@@ -1,40 +1,42 @@
-class AddressesController < ApplicationController
-  before_action :set_contact
-  
-  before_action :authenticate_user!
-  def create
-    @contact.address = Address.new(address_params)
+module V2
+  class AddressesController < ApplicationController
+    before_action :set_contact
+    
+    before_action :authenticate_user!
+    def create
+      @contact.address = Address.new(address_params)
 
-    if @contact.save
-      render json: @contact.address, status: :created, location: contact_address_url(@contact) 
-    else
-      render json: @contact.errors, status: :unprocessable_entity
+      if @contact.save
+        render json: @contact.address, status: :created, location: contact_address_url(@contact) 
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end 
+
+    # GET /contacts/1/address
+    def show
+      render json: @contact.address # Extraindo endereço
     end
-  end 
 
-  # GET /contacts/1/address
-  def show
-    render json: @contact.address # Extraindo endereço
+    def update
+      if @contact.address.update(address_params)
+        render json: @contact.address
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+    
+    def destroy
+      @contact.address.destroy
+    end
+
+    private
+      def address_params
+         ActiveModelSerializers::Deserialization.jsonapi_parse(params) # Deserializar os dados do json enviado
+      end
+
+      def set_contact
+        @contact = Contact.find(params[:contact_id])
+      end
   end
-
-  def update
-    if @contact.address.update(address_params)
-      render json: @contact.address
-    else
-      render json: @contact.errors, status: :unprocessable_entity
-    end
-  end
-  
-  def destroy
-    @contact.address.destroy
-  end
-
-  private
-    def address_params
-       ActiveModelSerializers::Deserialization.jsonapi_parse(params) # Deserializar os dados do json enviado
-    end
-
-    def set_contact
-      @contact = Contact.find(params[:contact_id])
-    end
 end
--- app/controllers/v2/phones_controller.rb
+++ app/controllers/v2/phones_controller.rb
@@ -1,43 +1,45 @@
-class PhonesController < ApplicationController
-  before_action  :set_contact
-  
-  before_action :authenticate_user!
-  def create
-    # Diferente do address voce precisa jogar o (has_many) objeto dentro da classe dona 
-    @contact.phones << Phone.new(phone_params)
+module V2
+  class PhonesController < ApplicationController
+    before_action  :set_contact
+    
+    before_action :authenticate_user!
+    def create
+      # Diferente do address voce precisa jogar o (has_many) objeto dentro da classe dona 
+      @contact.phones << Phone.new(phone_params)
 
-    if @contact.save
-      render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
-    else
-      render json: @contact.errors, status: :unprocessable_entity
-    end
-  end
-
-  def show
-    render json: @contact.phones
-  end
-  
-  def update
-    phone = Phone.find(phone_params[:id])
-
-    if phone.update(phone_params)
-     render json: @contact.phones
-    else
-     render json: @contact.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    phone = Phone.find(phone_params[:id])
-    phone.destroy
-  end
-
-  private
-    def set_contact
-      @contact = Contact.find(params[:contact_id])
+      if @contact.save
+        render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
     end
 
-    def phone_params
-      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+    def show
+      render json: @contact.phones
     end
+    
+    def update
+      phone = Phone.find(phone_params[:id])
+
+      if phone.update(phone_params)
+       render json: @contact.phones
+      else
+       render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      phone = Phone.find(phone_params[:id])
+      phone.destroy
+    end
+
+    private
+      def set_contact
+        @contact = Contact.find(params[:contact_id])
+      end
+
+      def phone_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+      end
+  end
 end
